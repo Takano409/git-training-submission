f5e787976b5793b6d112b5236a5b8891f69a7164
elean <elean@Rio>
2025-05-14 16:27:44 +0900

    Spring Boot + Thymeleafで新規登録画面を作成する

--- trainingspring/src/main/java/com/example/demo/controller/UserController.java
+++ trainingspring/src/main/java/com/example/demo/controller/UserController.java
@@ -1,23 +1,32 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.demo.dto.UserRequest;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 
+
 /**
- * user info Controller
+ * ユーザー情報 controller
  */
 @Controller
 public class UserController {
 	/**
-	 * user info Service
+	 * ユーザー情報　Service
 	 */
 	@Autowired
 	private UserService userService;
@@ -55,6 +64,7 @@ public class UserController {
 	 */
 	@GetMapping(value = "/user/add")
 	public String displayAdd(Model model) {
+		model.addAttribute("userRequest", new UserRequest());
 		return "user/add";
 	}
 	
@@ -66,14 +76,59 @@ public class UserController {
 	 */
 	@GetMapping("/user/{id}")
 	public String displayView(@PathVariable Long id, Model model) {
+		
 		return "user/view";
 	}
 	
-	
-	
-	
-	
-	
-	
-	
+	/*
+	 * ユーザー新規登録
+	 * @param useRequest リクエストデータ
+	 * @param model Model
+	 * @return ユーザー情報一覧画面
+	 */
+	@RequestMapping(value = "/user/create", method = RequestMethod.POST)
+	public String create(@Validated @ModelAttribute UserRequest userRequest, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			//入力チェックエラーの場合
+			List<String>errorList = new ArrayList<String>();
+			for(ObjectError error : result.getAllErrors()) {
+				errorList.add(error.getDefaultMessage());
+			}
+			
+			model.addAttribute("validationError", errorList);
+			return "user/add";
+		}
+		
+		/*
+		 * ユーザー情報の登録
+		 */
+		userService.create(userRequest);
+		return "redirect:/user/list";
+		
+		
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
