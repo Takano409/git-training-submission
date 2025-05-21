ca298faff8a941577b37d8bd9fb862cabbb2f4d5
elean <elean@host.docker.internal>
2025-05-21 15:31:14 +0900

    a

--- moattravel/src/main/java/com/example/moattravel/controller/AdminHouseController.java
+++ moattravel/src/main/java/com/example/moattravel/controller/AdminHouseController.java
@@ -6,12 +6,18 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.moattravel.entity.House;
+import com.example.moattravel.form.HouseRegisterForm;
 import com.example.moattravel.repository.HouseRepository;
 
 @Controller
@@ -26,7 +32,7 @@ public class AdminHouseController {
     }	
     
     @GetMapping            
-     public String index(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable, @RequestParam(name = "keyword", required = false) String keyword) {        
+     public String index(Model model, @PageableDefault(page = 0, size = 20, sort = "id", direction = Direction.ASC) Pageable pageable, @RequestParam(name = "keyword", required = false) String keyword) {        
         Page<House> housePage;
         
         if (keyword != null && !keyword.isEmpty()) {
@@ -49,24 +55,24 @@ public class AdminHouseController {
         
         return "admin/houses/show";
     }  
-//    
-//    @GetMapping("/register")
-//    public String register(Model model) {
-//        model.addAttribute("houseRegisterForm", new HouseRegisterForm());
-//        return "admin/houses/register";
-//    } 
-//    
-//    @PostMapping("/create")
-//    public String create(@ModelAttribute @Validated HouseRegisterForm houseRegisterForm, BindingResult bindingResult, RedirectAttributes redirectAttributes) {        
-//        if (bindingResult.hasErrors()) {
-//            return "admin/houses/register";
-//        }
-//        
+    
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("houseRegisterForm", new HouseRegisterForm());
+        return "admin/houses/register";
+    } 
+    
+    @PostMapping("/create")
+    public String create(@ModelAttribute @Validated HouseRegisterForm houseRegisterForm, BindingResult bindingResult, RedirectAttributes redirectAttributes) {        
+        if (bindingResult.hasErrors()) {
+            return "admin/houses/register";
+        }
+        
 //        houseService.create(houseRegisterForm);
-//        redirectAttributes.addFlashAttribute("successMessage", "民宿を登録しました。");    
-//        
-//        return "redirect:/admin/houses";
-//    } 
+        redirectAttributes.addFlashAttribute("successMessage", "民宿を登録しました。");    
+        
+        return "redirect:/admin/houses";
+    } 
 //    
 //    @GetMapping("/{id}/edit")
 //    public String edit(@PathVariable(name = "id") Integer id, Model model) {
