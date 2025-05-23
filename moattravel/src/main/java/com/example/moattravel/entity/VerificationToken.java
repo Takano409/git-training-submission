99b8a793094713347009ee3dc64ec9ba624dfdfb
elean <elean@host.docker.internal>
2025-05-23 19:01:26 +0900

    totyuu

---
+++ moattravel/src/main/java/com/example/moattravel/entity/VerificationToken.java
@@ -0,0 +1,39 @@
+package com.example.moattravel.entity;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.Data;
+
+
+
+@Entity
+@Table(name = "varification_tokens")
+@Data
+public class VerificationToken {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@OneToOne
+	@JoinColumn(name = "user_id")
+	@Column(name = "token")
+	private String token;
+	
+	@Column(name = "created_at", insertable = false, updatable = false)
+	private Timestamp createdAt;
+	
+	@Column(name = "updated_at", insertable = false, updatable = false)
+	private Timestamp updaatedAt;
+
+}
---
+++ moattravel/src/main/java/com/example/moattravel/event/SignupEventListener.java
@@ -0,0 +1,32 @@
+package com.example.moattravel.event;
+
+import java.util.UUID;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+import com.example.moattravel.entity.User;
+import com.example.moattravel.service.VerificationTokenService;
+
+@Component
+public class SignupEventListener {
+	private final VerificationTokenService verificationTokenService;
+	private final JavaMailSender javaMailSender;
+	
+	public SignupEventListener(VerificationTokenService verificationTokenService, JavaMailSender mailSender) {
+		this.verificationTokenService = verificationTokenService;
+		this.javaMailSender = mailSender;
+	}
+	
+	@EventListener
+	private void onSignupEvent(SignupEvent signupEvent) {
+		User user = signupEvent.getUser();
+		String token = UUID.randomUUID().toString();
+		verificationTokenSender.create(user, token);
+		
+		String resipientAddress = user.getEmail();
+		String subject
+	}
+	
+	
+}
