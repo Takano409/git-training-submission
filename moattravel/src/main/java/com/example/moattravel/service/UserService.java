34a4300cfb165e8b105e53c50055b5783c19f8f5
elean <elean@host.docker.internal>
2025-05-26 18:36:03 +0900

    27

--- moattravel/src/main/java/com/example/moattravel/service/UserService.java
+++ moattravel/src/main/java/com/example/moattravel/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.moattravel.entity.Role;
 import com.example.moattravel.entity.User;
 import com.example.moattravel.form.SignupForm;
+import com.example.moattravel.form.UserEditForm;
 import com.example.moattravel.repository.RoleRepository;
 import com.example.moattravel.repository.UserRepository;
 
@@ -40,19 +41,19 @@ public class UserService {
         return userRepository.save(user);
     } 
     
-//    @Transactional
-//    public void update(UserEditForm userEditForm) {
-//        User user = userRepository.getReferenceById(userEditForm.getId());
-//        
-//        user.setName(userEditForm.getName());
-//        user.setFurigana(userEditForm.getFurigana());
-//        user.setPostalCode(userEditForm.getPostalCode());
-//        user.setAddress(userEditForm.getAddress());
-//        user.setPhoneNumber(userEditForm.getPhoneNumber());
-//        user.setEmail(userEditForm.getEmail());      
-//        
-//        userRepository.save(user);
-//    }    
+    @Transactional
+    public void update(UserEditForm userEditForm) {
+        User user = userRepository.getReferenceById(userEditForm.getId());
+        
+        user.setName(userEditForm.getName());
+        user.setFurigana(userEditForm.getFurigana());
+        user.setPostalCode(userEditForm.getPostalCode());
+        user.setAddress(userEditForm.getAddress());
+        user.setPhoneNumber(userEditForm.getPhoneNumber());
+        user.setEmail(userEditForm.getEmail());      
+        
+        userRepository.save(user);
+    }    
     
     // メールアドレスが登録済みかどうかをチェックする
     public boolean isEmailRegistered(String email) {
@@ -73,8 +74,8 @@ public class UserService {
     } 
     
     // メールアドレスが変更されたかどうかをチェックする
-//    public boolean isEmailChanged(UserEditForm userEditForm) {
-//        User currentUser = userRepository.getReferenceById(userEditForm.getId());
-//        return !userEditForm.getEmail().equals(currentUser.getEmail());        
-//    }    
+    public boolean isEmailChanged(UserEditForm userEditForm) {
+        User currentUser = userRepository.getReferenceById(userEditForm.getId());
+        return !userEditForm.getEmail().equals(currentUser.getEmail());        
+    }    
 }
